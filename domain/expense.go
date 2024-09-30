fde371179e1ca398a1dcdfaf783cb3329c4efec8
dat <snapeandcandy@gmail.com>
2024-09-30 22:49:59 +0700

    update domain

--- domain/expense.go
+++ domain/expense.go
@@ -1,8 +1,9 @@
 package domain
 
 type Expense struct {
-	ID          int64   `json:id`
-	Name        string  `json:name`
-	Description string  `json:description`
-	Amount      float64 `json:amount`
+	ID          int64   `json:"id"`
+	User_ID     int64   `json:"user_id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Amount      float64 `json:"amount"`
 }
